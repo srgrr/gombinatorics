5709dd6be70c22df06a4f52648eb75970d7ed04b
Sergio <sergio.r.guasch@gmail.com>
2025-08-09 21:49:54 +0200

    fix: simplify example

--- examples/cartesian_product.go
+++ examples/cartesian_product.go
@@ -2,22 +2,17 @@ package main
 
 import (
 	"fmt"
-	"sync"
+
 	sets "github.com/srgrr/gombinatorics/sets"
 )
-
-func sendSpam(user string, spam string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("Sent spam\t\"%s\"\t to %s,\thope they will buy my stuff!\n", spam, user)
-}
 
 func main() {
 	emails := []string{"sergio@raccoon.me", "raquel@cat.me"}
 	spam := []string{"Raccoon plushies now 10% discount", "Brown hair dye now 5% discount"}
-	var wg sync.WaitGroup
 	for emailAndSpam := range sets.CartesianProduct(emails, spam) {
-		wg.Add(1)
-		go sendSpam(emailAndSpam.First, emailAndSpam.Second, &wg)
+		fmt.Printf(
+			"%s got spammed with \"%s\"\n",
+			emailAndSpam.First, emailAndSpam.Second,
+		)
 	}
-	wg.Wait()
 }
