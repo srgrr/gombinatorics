0872943ea58265535f4d84db410e4988174e4590
Sergio <sergio.r.guasch@gmail.com>
2025-08-13 22:42:39 +0200

    feat: add CPartition

--- functional/partition.go
+++ functional/partition.go
@@ -22,3 +22,34 @@ func Partition[T any](ctx context.Context, A []T, k int) <-chan []T {
 	}()
 	return ch
 }
+
+// CPartition channels consecutive slices of size k from a read-only channel
+// except for maybe the last, which can be of size n % k
+// Zero and negative numbers are not accepted here
+// It is similar to Partition but works with channels instead of arrays
+func CPartition[T any](ctx context.Context, A <-chan T, k int) <-chan []T {
+	if k < 1 {
+		panic("k must be at least 1")
+	}
+	ch := make(chan []T)
+	go func() {
+		defer close(ch)
+		batch := make([]T, 0, k)
+		for elem := range A {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				batch = append(batch, elem)
+				if len(batch) == k {
+					ch <- batch
+					batch = make([]T, 0, k)
+				}
+			}
+		}
+		if len(batch) > 0 {
+			ch <- batch
+		}
+	}()
+	return ch
+}
--- functional/partition_test.go
+++ functional/partition_test.go
@@ -23,3 +23,31 @@ func TestPartition(t *testing.T) {
 		t.Errorf("Expected %s\nReceived %s", expected, received)
 	}
 }
+
+func TestCPartition(t *testing.T) {
+	ctx := context.Background()
+	strings := []string{"the", "quick", "brown", "fox", "jumps", "over", "the"}
+	received := make([][]string, 0)
+
+	supplierChannel := make(chan string)
+	go func() {
+		defer close(supplierChannel)
+		for _, str := range strings {
+			supplierChannel <- str
+		}
+	}()
+
+	for partition := range CPartition(ctx, supplierChannel, 2) {
+		received = append(received, partition)
+	}
+
+	expected := [][]string{
+		{"the", "quick"},
+		{"brown", "fox"},
+		{"jumps", "over"},
+		{"the"},
+	}
+	if !reflect.DeepEqual(expected, received) {
+		t.Errorf("Expected %s\nReceived %s", expected, received)
+	}
+}
