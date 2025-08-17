651259de88d7efe788d7f6c572c0b5a386d3b8a1
Sergio Raccoon Guasch <sergio@openfort.xyz>
2025-08-17 16:06:02 +0200

    feat: add step range

--- functional/range.go
+++ functional/range.go
@@ -18,3 +18,23 @@ func Range(ctx context.Context, l, r int) <-chan int {
 	}()
 	return ch
 }
+
+// StepRange channels integers in [l, r) with a step
+// It is similar to the built-in range function but works with channels
+func StepRange(ctx context.Context, l, r, step int) <-chan int {
+	if step <= 0 {
+		panic("step must be greater than 0")
+	}
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for i := l; i < r; i += step {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- i:
+			}
+		}
+	}()
+	return ch
+}
