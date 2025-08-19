c464bca051804341969284d7b04314facc21bbf5
Sergio Raccoon Guasch <sergio@openfort.xyz>
2025-08-19 11:13:19 +0200

    feat: extend repeat to support infinite sequences

--- repeat.go
+++ repeat.go
@@ -4,16 +4,19 @@ import "context"
 
 // Repeat channels the same element k times
 // It is similar to the built-in repeat function but works with channels
-// Zero and negative numbers are not accepted here
 // If k is 0, it returns an empty channel
+// If k is -1, it repeats indefinitely
+// If k is positive, it repeats exactly k times
+// Other values of k are not accepted
+// It is similar to the built-in repeat function but works with channels
 func Repeat[T any](ctx context.Context, elem T, k int) chan T {
-	if k < 0 {
-		panic("k must be at least 0")
+	if k < -1 {
+		panic("k must be -1 for infinite repetition, 0 for empty channel, or a positive integer")
 	}
 	ch := make(chan T)
 	go func() {
 		defer close(ch)
-		for i := 0; i < k; i++ {
+		for i := 0; i == -1 || i < k; i++ {
 			select {
 			case <-ctx.Done():
 				return
