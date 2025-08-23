d6d9931a6fb0e6d5fe1d6c8bb0e4e2c3229ca685
Sergio Raccoon Guasch <sergio@openfort.xyz>
2025-08-23 20:45:41 +0200

    fix: add docs for channelize

--- channelize.go
+++ channelize.go
@@ -2,6 +2,7 @@ package gombinator
 
 import "context"
 
+// Channelize channels the elements of a slice
 func Channelize[T any](ctx context.Context, A []T) <-chan T {
 	return Map(ctx, A, func(a T) T { return a })
 }
