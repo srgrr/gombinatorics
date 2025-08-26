d9bea3d8611f11af6b4de0353a2a606c0a18495b
Sergio Raccoon Guasch <sergio@openfort.xyz>
2025-08-26 08:43:33 +0200

    test: test CZip w/ remnant

--- zip_test.go
+++ zip_test.go
@@ -27,3 +27,25 @@ func TestZip(t *testing.T) {
 		t.Errorf("Error:\nGot\t%+v\nExpected\t%+v", zippedPairs, expected)
 	}
 }
+
+func TestCZip(t *testing.T) {
+	ctx := context.Background()
+	cities := Channelize(ctx, []string{"london", "sf", "philly"})
+	weather := Channelize(ctx, []string{"cloudy", "foggy", "crazy", "remnant"})
+
+	zippedPairs := make([]Pair[string, string], 0)
+
+	for pair := range CZip(ctx, cities, weather) {
+		zippedPairs = append(zippedPairs, pair)
+	}
+
+	expected := []Pair[string, string]{
+		{First: "london", Second: "cloudy"},
+		{First: "sf", Second: "foggy"},
+		{First: "philly", Second: "crazy"},
+	}
+
+	if !reflect.DeepEqual(expected, zippedPairs) {
+		t.Errorf("Error:\nGot\t%+v\nExpected\t%+v", zippedPairs, expected)
+	}
+}
